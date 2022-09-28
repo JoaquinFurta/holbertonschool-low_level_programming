e248f9526ea6d18bf6a786cd80fb4f961b8be27b
root <joaquinfurta2@gmail.com>
2022-09-28 09:01:49 -0700

    32

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -12,10 +12,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
-		puts("is negative");
+		printf("is negative");
 	else if (n > 0)
-		puts("is positive");
+		printf("is positive");
 	else
-		puts("is zero");
+		printf("is zero");
 	return (0);
 }
