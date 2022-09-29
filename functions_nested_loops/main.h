545580758a6b9f315d0b1e4ce8d6227b592faa08
JoaquinFurta <joaquinfurta2@gmail.com>
2022-09-29 08:48:12 -0700

    2

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,17 +1,7 @@
 #include <stdio.h>
-
+#include <unistd.h>i
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
 int print_alphabet(void);
 int _islower(int c);
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
