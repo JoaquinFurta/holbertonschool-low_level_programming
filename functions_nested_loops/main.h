efcb30072655b3d1becf91bfc5533dc5f8dd54a7
JoaquinFurta <joaquinfurta2@gmail.com>
2022-09-29 08:00:04 -0700

    2

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,14 +4,14 @@ int print_alphabet(void);
 int _islower(int c);
 int main(void)
 {
-	    int r;
+	int r;
 
-	        r = _islower('H');
-		    _putchar(r + '0');
-		        r = _islower('o');
-			    _putchar(r + '0');
-			        r = _islower(108);
-				    _putchar(r + '0');
-				        _putchar('\n');
-					    return (0);
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
 }
