3494904ab82ce49c14394aaebb7a257a88025bfb
JoaquinFurta <joaquinfurta2@gmail.com>
2022-09-29 07:53:30 -0700

    2

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - xd
+ * Return: 0, 1
+ */
+int _islower(int c)
+{
+	char i;
+	int resultado;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		if (c == i)
+			resultado = 1;
+		else
+			resultado = 0;
+	}
+	return (resultado);
+}
