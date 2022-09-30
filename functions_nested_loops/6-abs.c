8934a8bf81d348ce4d45d9069d4fa12a8aa315dc
JoaquinFurta <joaquinfurta2@gmail.com>
2022-09-30 08:46:32 -0700

    2

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - xd
+ * Return: 0, 1
+ * @n : xddd
+ */
+int _abs(int n)
+{
+	if (n <= 0)
+		return (-n);
+	else
+		return (n);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int _putchar(char c);
 int print_alphabet_x10(void);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int n);
