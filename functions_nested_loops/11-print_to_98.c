7bcf871ff053601501baa049f083ce2135902d63
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-01 14:32:16 -0700

    f

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - xd
+ * Return: 0
+ * @n: xd
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		for (n ; n >= 98; n--)
+		{
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d ", n);
+		}
+	}
+	else
+	{
+		for (n ; n <= 98; n++)
+			if (n == 98)
+				printf("%d", n);
+			else
+				printf("%d ", n);
+	}
+}
