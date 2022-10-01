ad703bb777b49867ddc115fb792883e43fab440b
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-01 14:43:10 -0700

    f

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,23 +7,25 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n >= 98)
 	{
-		for (n ; n >= 98; n--)
+		for (i = n ; i >= 98; i--)
 		{
-			if (n == 98)
-				printf("%d", n);
+			if (i == 98)
+				printf("%d", i);
 			else
-				printf("%d ", n);
+				printf("%d ", i);
 		}
 	}
 	else
 	{
-		for (n ; n <= 98; n++)
-			if (n == 98)
-				printf("%d", n);
+		for (i = n ; i <= 98; i++)
+			if (i == 98)
+				printf("%d", i);
 			else
-				printf("%d ", n);
+				printf("%d ", i);
 	}
-	printf("\n")
+	printf("\n");
 }
