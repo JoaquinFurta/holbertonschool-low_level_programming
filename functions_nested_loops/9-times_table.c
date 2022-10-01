bfc05679e89be76151739312ad8c47b96c4883f6
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-01 12:44:49 -0700

    4

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - xd
+ * Return: 0,
+ */
+void times_table(void)
+{
+	int h, m, f;
+
+	for (h = 0; h < 11; h++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (m = 1; m < 11; m++)
+		{
+			f = h * m;
+			if (f / 10 == 0)
+				_putchar(' ');
+			else
+				_putchar (f / 10 + '0');
+				_putchar (f % 10 + '0');
+				_putchar (',');
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
