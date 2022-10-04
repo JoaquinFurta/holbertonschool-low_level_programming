15cd7c15c1e5f1f6bc995acfb330d78497fd79d2
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-04 09:46:07 -0700

    2

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -6,9 +6,15 @@
 void print_square(int size)
 {
 	int i;
+	int x;
 
 	if (size > 0)
-		for (i = 1 ; i <= size; i++)
-			_putchar('#');
-	_putchar('\n');
+		for (x = 1; x <= size; x++)
+		{
+			for (i = 1; i <= size; i++)
+				_putchar('#');
+		_putchar('\n');
+		}
+	else
+		_putchar('\n');
 }
