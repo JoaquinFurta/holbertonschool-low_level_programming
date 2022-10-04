5870e5ed36528d03ca25fe1213f33bbc997dccc3
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-04 05:48:57 -0700

    2

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _isdigit - xd
+ * Return: 1,0
+ * @c : integer
+ */
+int _isdigit(int c);
+{
+	int i;
+	int resultado;
+
+	resultado = 0;
+	for (i = '0'; i <= '9'; i++)
+		if (c == i)
+			resultado = 1;
+	return (resultado);
+}
