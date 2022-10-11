6b58ca413e8130c10e9fcc5fd989936e53500a04
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-11 12:52:10 -0700

    22

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -3,13 +3,13 @@
  * _puts2 - xd
  * @str : des
  */
-void _puts2(char *str)
+void _puts2(char *stri)
 {
 	int i;
 
 	for (i = 0 ; str[i] != '\0'; i++)
 	{
-		if( i % 2 == 0)
+		if (i % 2 == 0)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
