d9cf8f08a247b1972eb8b9bee573412ab61898be
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-11 12:24:23 -0700

    22

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - xd
+ * @s : des
+ */
+void rev_string(char *s)
+{
+	int i;
+	int contador;
+	char var;
+
+	contador = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+		contador++;
+	i = 0;
+	contador--;
+	while (contador-- > i)
+	{
+		var = s[contador];
+		s[contador] = s[i];
+		s[i] = s[contador];
+		i--;
+		contador++;
+	}
+}
