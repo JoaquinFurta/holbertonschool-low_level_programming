3252bc1bf507079be912f60b7dee237ea79a9776
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-11 09:55:08 -0700

    2

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - xd
+ * @s : des
+ * Return: int
+ */
+void print_rev(char *s)
+{
+	int i;
+	int contador;
+
+	contador = 0;
+
+	for (i = 0 ; s[i] != '\0'; i++)
+		contador++;
+	for (i = contador; s[i] == s[0]; i--)
+		_putchar(s[i]);
+}
