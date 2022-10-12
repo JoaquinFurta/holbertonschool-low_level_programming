9cb4c7dab95a1e869bebb461e4dfb125b81a447f
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-12 07:27:46 -0700

    2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,10 +9,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int largo = 0;
+	int len = 0;
 
+	for (; dest[len]; len++);
 	for (; largo < n && src[largo]; largo++)
-		dest[largo] = src[largo];
+		dest[largo + len] = src[largo];
 
-	dest[largo] = '\0';
+	dest[largo + len] = '\0';
 	return (dest);
 }
