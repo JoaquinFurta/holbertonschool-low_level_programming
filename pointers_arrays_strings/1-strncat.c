b445fea2190836069254dbb7ff50d1fb2f3168c0
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-12 07:37:01 -0700

    2

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -3,18 +3,25 @@
  * _strncat - xd
  * @src: des
  * @dest: ss
- * @n: dede
+ * @n: dsd
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n);
 {
+	int i;
+	int contador = 0;
 	int largo = 0;
-	int len = 0;
 
-	for (; dest[len]; len++);
-	for (; largo < n && src[largo]; largo++)
-		dest[largo + len] = src[largo];
+	for (i = 0; src[i] != '\0'; i++)
+		contador++;
 
-	dest[largo + len] = '\0';
+	for (i = 0; dest[i] != '\0'; i++)
+		largo++;
+
+
+	for (i = 0; i < n; i++)
+		dest[i + largo] = src[i];
+
+	dest[largo + contador] = '\0';
 	return (dest);
 }
