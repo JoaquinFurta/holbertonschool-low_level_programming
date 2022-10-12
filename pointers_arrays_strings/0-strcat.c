bb5c1d33d15876b80b84efbcf225b07abd174d52
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-12 05:55:59 -0700

    22

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - xd
+ * @src: des
+ * @dest: ss
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int contador = 0;
+	int largo = 0;
+
+	for (i = 0; src[i] != '\0'; i++)
+		contador++;
+	
+	for (i = 0; dest[i] != '\0'; i++)
+		largo++;
+
+
+	for (i = 0; i < contador; i++)
+		dest[i + largo] = src[i];
+
+	dest[largo + contador - 1] = '\0';
+	return (dest);
+}
