7ced61665201f6e073d47385c15a934d1994781a
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-13 16:09:31 -0700

    22

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* reverse_array - dd
+* @a: xd
+* @n: dea
+*/
+void reverse_array(int *a, int n)
+{
+	int aux, i;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		aux = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = aux;
+	}
+}
