3bd6e4cc6812a8e1ddff7b263557fa29adefca0c
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-13 14:01:54 -0700

    22

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - xd
+ * @src: des
+ * @dest: ss
+ * @n: dsd
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+{
+	int i = 0
+
+	for (i = 0; src[i] && i <= n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
