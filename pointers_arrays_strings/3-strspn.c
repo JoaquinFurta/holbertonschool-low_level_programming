6e102853055adb51e23baddd0ee510970847e4f0
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-13 18:19:53 -0700

    2

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - fsdfds
+ * @s: fdfd
+ * @accept: dd
+ * Return: d
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int i = 0;
+
+	while ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				bytes++;
+		}
+
+		s++;
+	}
+
+	return (bytes)
