652af690414766fba26563b0c599f41f5c74c22b
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-13 17:48:56 -0700

    2

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+ #include "main.h"
+
+/**
+ * _memcpy - ds
+ * @dest: xd
+ * @src: xd
+ * @n: ff
+ * Return:d
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
