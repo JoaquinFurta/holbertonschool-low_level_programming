7155862f263a8c68fa326a0aa7dd94ed9d55d5a6
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 09:11:19 -0700

    2-strlen_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,10 +8,13 @@
  */
 int _pow_recursion(int x, int y)
 {
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
-	if (y == 0 )
-		return;
+	if (y <= 1)
+	{
+	}
 	else
-		return (n * factorial(n, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 }
