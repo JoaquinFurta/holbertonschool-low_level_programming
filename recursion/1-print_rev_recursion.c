d05898676776bba99b47757af1ea651c2202e000
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 07:32:08 -0700

    2

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_rev_recursion - xd
+ * @s : fdf
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0') 
+	{
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
