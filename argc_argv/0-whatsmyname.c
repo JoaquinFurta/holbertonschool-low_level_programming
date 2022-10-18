25e7848eb203ff408b6c86b584b5f4b7a8ba0e97
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 13:37:04 -0700

    2

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - dsds
+ * @argc: ds
+ * @argv: fd
+ * Return: ddd
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
