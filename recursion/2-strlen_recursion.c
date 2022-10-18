bd74f4564a4a55b49df7e1c05f7bc9e4e445c6e3
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 08:06:53 -0700

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - xd
+ * @s : fdf
+ */
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+		return (0);
+	else
+		return(1 + _print_rev_recursion(s + 1);
+}
