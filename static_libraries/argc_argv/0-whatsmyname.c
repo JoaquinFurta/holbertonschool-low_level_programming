95a191151eb64f02ac5ccfb674c28e1c0ebee5b8
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 13:33:43 -0700

    2

--- static_libraries/argc_argv/0-whatsmyname.c
+++ static_libraries/argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - fdfd
+ * main - dsds
  * @argc: ds
  * @argv: fd
+ * Return: ddd
  */
-int main (__attribute__((unused)) int argc, char* argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
