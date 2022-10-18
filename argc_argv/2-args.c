353b2762b3124cc4e49898f1eafada6796b67fe2
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-18 14:02:25 -0700

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - dsds
+ * @argc: ds
+ * @argv: fd
+ * Return: ddd
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
