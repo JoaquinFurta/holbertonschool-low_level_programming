851edfcbe8f19a31a6dd81eb117c18a9d4f464d6
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-20 08:08:44 -0700

    2

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strdup - creates an array of chars
+ * @str: char
+ * Return: a copy of the stirng
+ */
+char *_strdup(char *str)
+{
+	int i, mem = 0;
+	char *p;
+
+	if (str == NULL)
+		return ('\0');
+
+	while (str[mem] != '\0')
+		mem++;
+
+	p = malloc(mem++);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i <= mem; i++)
+		p[i] = str[i];
+
+	return (p);
+	free(p);
+}
