77b76271aa09dc824c98fb29f671790bb21c7621
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-24 15:15:23 -0700

    task3

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * array_range - array of int with a range as a parameter
+ * @min: min
+ * @max: max
+ * Return: an array of integeres
+ */
+
+int *array_range(int min, int max)
+{
+	int *p, len = 0;
+
+	if (min > max)
+		return (NULL);
+
+	for (; min <= max; min++)
+		len++;
+
+	p = malloc(len * 4);
+
+	if (p == NULL)
+		return (NULL);
+
+	return (p);
+}
