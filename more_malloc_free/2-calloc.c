d3a572efb4547ef451f71f3da045f1ca9d3f69ec
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-24 13:39:05 -0700

    fix

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,13 +10,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i;
+	char *aux;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(nmemb * size);
 
-	if (p == NULL)
+	if (p != NULL)
+	{	
+		aux = p;
+		for (i = 0; i < nmemb * size; i++)
+			aux[i] = '\0';
+	}
+	else
 		return (NULL);
 	return (p);
 }
