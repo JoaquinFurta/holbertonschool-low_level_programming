67100cae9c8d30b35639fada2cc46e2bb3f547d5
JoaquinFurta <joaquinfurta2@gmail.com>
2022-10-31 10:31:10 -0700

    2

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+#define dog
+#include <stdio.h>
+
+/**
+ * struct dog - dog
+ * @char: name of the dog
+ * @age: age of the dog
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
