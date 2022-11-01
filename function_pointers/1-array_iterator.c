9b8af46e71c06806c6d405cd51d392736c16f71e
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-01 10:19:04 -0700

    2

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints a name
+ * @array: name of the person
+ * @size: size of the array
+ * @action: another function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	int i;
+
+	if (action != NULL)
+		for (i = 0; i <= size, i++)
+			action(array[i]);
+}
