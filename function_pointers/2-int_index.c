ec82c057221c7111c2740724991c947532a354ca
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-01 12:06:03 -0700

    2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -12,8 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0, resultado = -1;
 
-	if (size <= 0)
-		return (0);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
 	while (i < size)
 	{
 		if (cmp(array[i]) == 0)
