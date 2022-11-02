3bf2acd37e8c31aa9e2ac1893eeabb4ffec0db18
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-01 17:45:01 -0700

    task_0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - reurns the additions of all its parameteres
+ * @n: number of paramters
+ * @...: variable number of parameters
+ * Return: addiion
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i; suma = 0;
+	va_list n_para;
+
+	va_start(n_para,n);
+
+	for (i = 0; i < n; i++)
+		sum = va_arg(n_para,int) + suma;
+
+	va_end(n_para);
+
+	return (suma);
+}
