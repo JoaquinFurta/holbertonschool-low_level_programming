e1713be3a620f0fa8ac8842481e3844ccda196c0
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-01 17:52:35 -0700

    task_0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -12,10 +12,10 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i, suma = 0;
 	va_list n_para;
 
-	va_start(n_para,n);
+	va_start(n_para, n);
 
 	for (i = 0; i < n; i++)
-		suma = va_arg(n_para,int) + suma;
+		suma = va_arg(n_para, int) + suma;
 
 	va_end(n_para);
 
