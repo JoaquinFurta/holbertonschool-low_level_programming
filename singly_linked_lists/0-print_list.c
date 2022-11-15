560a7bb12971294daca8b8a74b520a2d46650d25
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-15 06:48:43 -0800

    2

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints the list
+ * @h: header of the list
+ * Return: Number of nodes of the list
+ */
+size_t print_list(const list_t *h)
+{
+	int cont = 0;
+
+	while (h->next != NULL)
+	{
+		printf(h->str);
+		cont++;
+	}
+	return (cont);
+}
