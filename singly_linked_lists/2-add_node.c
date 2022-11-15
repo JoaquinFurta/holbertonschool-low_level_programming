079c024b723d698ae5dfe0a7963af3514cc5c36c
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-15 07:46:53 -0800

    2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - prints the list
+ * @head: header of the list
+ * @str: string
+ * Return: Number of nodes of the list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *aux;
+
+	aux = malloc(sizeof(list_t));
+	if (aux == NULL)
+		return (NULL);
+
+	aux->str = strdup(str);
+	aux->len = strlen(str);
+	aux->next = *head;
+	head = &aux;
+	return (head);
+}
