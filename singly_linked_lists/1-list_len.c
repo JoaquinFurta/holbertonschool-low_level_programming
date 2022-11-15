f75f910527755ffc0304da4c53e54399477184a7
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-15 07:14:02 -0800

    task1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - prints the list
+ * @h: header of the list
+ * Return: Number of nodes of the list
+ */
+size_t list_len(const list_t *h);
+{
+	int cont = 0;
+
+	while (h != NULL)
+	{
+		cont++;
+		h = h->next;
+	}
+	return (cont);
+}
