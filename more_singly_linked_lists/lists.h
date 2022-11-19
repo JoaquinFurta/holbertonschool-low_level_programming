8c60a6bb3121138623faa695e7f438989fdbbee4
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-19 15:55:09 -0800

    list

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -13,7 +13,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
+listint_t *_create_list(unsigned int n, ...);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
