9f376620bb4c9c622e2f5cd39895c44716eb3a5b
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-19 14:24:44 -0800

    task_0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints the list
+ * @h: header of the list
+ * Return: Number of nodes of the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	int cont = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		cont++;
+		h = h->next;
+	}
+	return (cont);
+}
