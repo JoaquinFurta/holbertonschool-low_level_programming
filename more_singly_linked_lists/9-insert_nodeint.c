9e6c6111e9dd4cebdadf59bbae6ec77227a4536b
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-20 12:35:24 -0800

    2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		mem++;
 	}
 	temp = malloc(sizeof(*head));
-	if ((temp == NULL) || (aux == NULL))
+	if (temp == NULL)
 	{
 		free(temp);
 		return (NULL);
