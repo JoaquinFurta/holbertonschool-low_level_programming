b0d8f312e4d1a249c8b8174af2003d3f368fdc78
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-20 13:02:37 -0800

    2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,7 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	aux = *head;
 
-	while ((aux != NULL) && (mem <= idx))
+	while ((aux != NULL) && (mem <= idx + 1))
 	{
 		aux = aux->next;
 		mem++;
