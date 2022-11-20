e0825e2833f86b0c768c420a3b404c3feea11f9f
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-19 19:44:26 -0800

    2

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int mem = 1;
-	listint_t *temp = NULL;
+	listint_t *temp;
 
 	while ((*head != NULL) && (mem <= idx - 1))
 	{
@@ -35,5 +35,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp->next = (*head)->next;
 		(*head)->next = temp;
 	}
-	return (*head);
+	return (temp);
 }
