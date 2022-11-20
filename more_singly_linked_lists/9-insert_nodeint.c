c5ba23cd569985bc9777ee3d257eafd0ed71e7a5
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-20 12:34:25 -0800

    task

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,13 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	aux = *head;
 
-	while ((aux != NULL) && (mem <= idx - 1))
+	while (mem <= idx - 1)
 	{
 		aux = aux->next;
 		mem++;
 	}
 	temp = malloc(sizeof(*head));
-	if (temp == NULL)
+	if ((temp == NULL) || (aux == NULL))
 	{
 		free(temp);
 		return (NULL);
