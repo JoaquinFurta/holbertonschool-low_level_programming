991f5adfd6e843883ad42bf76bf60d371b31a60e
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-19 16:52:20 -0800

    2

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -2,30 +2,27 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * free_listintxd - Realease the memory allocated for a list
- * @head: A pointer to the first node of the list
- * Return: 0 if success
- */
-
-int free_listintxd(listint_t *head)
-{
-	if (head)
-	{
-		free_listintxd(head->next);
-		free(head);
-	}
-	return (0);
-}
-
-/**
  * free_listint2 - Realease the memory allocated for a list, sets head to null
  *
  * @head: A pointer to the first node of the list
  */
+
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	listint_t *temp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+	}
+	else
 		return;
-	free_listintxd(*head);
+
+	free(*head);
 	head = NULL;
 }
