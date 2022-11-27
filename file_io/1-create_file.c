8f5f8d953aaa186935adee3fba3e362874035baa
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 14:53:29 -0800

    2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	o = open(filename, O_CREAT, O_WRONLY, 0600);
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	write(o, text_content, strlen(text_content));
 
 	if (o == -1)
