a3c67b5067299756ddc647f247bc351e55ff1632
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 15:12:25 -0800

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 		len = strlen(text_content);
 
-	o = open(filename, O_WRONLY);
+	o = open(filename, O_WRONLY | O_APPEND);
 	write(o, text_content, len);
 
 	if (o == -1)
