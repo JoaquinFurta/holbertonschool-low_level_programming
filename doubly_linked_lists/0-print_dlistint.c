c275a68079c68c695fb84c341ebf646c2ac34adf
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 17:57:56 -0800

    fst

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the list
+ * @h: header of the list
+ * Return: Number of nodes of the list
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	size_t cont = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		cont++;
+		h = h->next;
+	}
+	return (cont);
+}
