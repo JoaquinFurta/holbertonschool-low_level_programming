90bb156ba1eaaa8d46a83925e8f81dde270bff57
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 17:58:30 -0800

    fix

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,7 @@
  * @h: header of the list
  * Return: Number of nodes of the list
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t cont = 0;
 
