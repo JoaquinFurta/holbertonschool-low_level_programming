6601c3471cd71659e806765826f693e2f58076d7
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 18:32:48 -0800

    task_4

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,11 +7,11 @@
  *
  * @head: A pointer to the first node of the list
  */
-void free_dlistint(listint_t *head)
+void free_dlistint(dlistint_t *head)
 {
 	if (head)
 	{
-		free_listint(head->next);
+		free_dlistint(head->next);
 		free(head);
 	}
 }
