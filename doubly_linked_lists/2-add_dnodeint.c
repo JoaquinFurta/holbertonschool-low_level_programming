95ca6b697a809954e0173df3a6a596e096a39dc9
JoaquinFurta <joaquinfurta2@gmail.com>
2022-11-27 18:16:29 -0800

    2-add_dnodeint.c

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - prints the list
+ * @head: header of the list
+ * @n: integer
+ * Return: Number of nodes of the list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *aux;
+
+	aux = malloc(sizeof(dlistint_t));
+	if (aux == NULL)
+	{
+		free(aux);
+		return (NULL);
+	}
+
+	aux->n = n;
+	aux->next = *head;
+	(*head)->prev = aux;
+	aux->prev = NULL;
+	*head = aux;
+
+
+	return (*head);
+}
