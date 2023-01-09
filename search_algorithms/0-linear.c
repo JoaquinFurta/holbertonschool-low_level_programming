4534548a9049d4b1cbf66de9c212ebca285de607
JoaquinFurta <joaquinfurta2@gmail.com>
2023-01-09 12:59:14 -0800

    task_0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - search a value in an array,using the linear search algorithm
+ * @array: array to search value
+ * @size: size of the array
+ * @value: integer to search
+ * Return: index of the value, (-1) if it fails
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
